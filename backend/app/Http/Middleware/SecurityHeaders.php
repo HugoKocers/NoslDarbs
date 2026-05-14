9592ca5302d6d6bb2e70a158a7081676a31ae00f
Hugo Kocers <166509365+HugoKocers@users.noreply.github.com>
2026-05-14 20:08:54 +0300

    Update SecurityHeaders.php

--- backend/app/Http/Middleware/SecurityHeaders.php
+++ backend/app/Http/Middleware/SecurityHeaders.php
@@ -22,7 +22,7 @@ class SecurityHeaders
         $response->header('X-Frame-Options', 'DENY');
         $response->header('X-XSS-Protection', '1; mode=block');
         $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-       $response->header('Content-Security-Policy', "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; script-src 'self'; img-src 'self' data: https:; font-src 'self' https://fonts.gstatic.com; connect-src 'self' http://localhost:8000 http://127.0.0.1:8000 https://nosldarbs-production.up.railway.app;");
+        $response->header('Content-Security-Policy', "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; script-src 'self'; img-src 'self' data: https:; font-src 'self' https://fonts.gstatic.com; connect-src 'self' http://localhost:8000 http://127.0.0.1:8000 https://nosldarbs-production.up.railway.app;");
         $response->header('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->header('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
 
